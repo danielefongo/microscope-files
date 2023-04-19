3ec267f15f4c2f61455e922c0bb18bb875d02c63
danielefongo <danielefongo@gmail.com>
2023-04-19 09:17:59 +0200

    fix: do not open again already opened file

--- lua/microscope-files/open.lua
+++ lua/microscope-files/open.lua
@@ -1,7 +1,11 @@
 return function(data, win, _)
   assert(data.file, "Provide file field")
 
-  vim.cmd("e " .. data.file)
+  local full_path = vim.fn.fnamemodify(data.file, ":p")
+  local buffer = vim.fn.bufnr(full_path, true)
+  vim.api.nvim_buf_set_option(buffer, "buflisted", true)
+  vim.api.nvim_win_set_buf(win, buffer)
+
   if data.row and data.col then
     local cursor = { data.row, data.col }
     vim.api.nvim_win_set_cursor(win, cursor)
