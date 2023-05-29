8cb4c4c54b818df48fb280eb8fe2f872b92f0373
danielefongo <danielefongo@gmail.com>
2023-05-29 11:04:58 +0200

    feat: extend the number of the retrieved results

--- lua/microscope-files/finders.lua
+++ lua/microscope-files/finders.lua
@@ -7,31 +7,31 @@ local parsers = require("microscope.builtin.parsers")
 
 return {
   workspace_grep = {
-    lens = lenses.head(100, file_lenses.vimgrep()),
+    lens = lenses.head(5000, file_lenses.vimgrep()),
     open = open,
     preview = preview.cat,
     parsers = { file_parsers.file_row_col, file_parsers.regex },
   },
   workspace_fuzzy = {
-    lens = lenses.head(100, lenses.fzf(file_lenses.prefiltered_all_lines())),
+    lens = lenses.head(500, lenses.fzf(file_lenses.prefiltered_all_lines())),
     open = open,
     preview = preview.cat,
     parsers = { file_parsers.file_row_col, parsers.fuzzy },
   },
   buffer_grep = {
-    lens = lenses.head(100, file_lenses.buffergrep()),
+    lens = lenses.head(5000, file_lenses.buffergrep()),
     open = open,
     preview = preview.cat,
     parsers = { file_parsers.row_col, file_parsers.regex },
   },
   buffer_fuzzy = {
-    lens = lenses.head(100, lenses.fzf(lenses.cache(file_lenses.buffer_lines()))),
+    lens = lenses.head(500, lenses.fzf(lenses.cache(file_lenses.buffer_lines()))),
     open = open,
     preview = preview.cat,
     parsers = { file_parsers.row_col, parsers.fuzzy },
   },
   file = {
-    lens = lenses.head(100, lenses.fzf(lenses.cache(file_lenses.rg()))),
+    lens = lenses.head(5000, lenses.fzf(lenses.cache(file_lenses.rg()))),
     open = open,
     preview = preview.cat,
     parsers = { file_parsers.file, parsers.fuzzy },
