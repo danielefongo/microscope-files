8d69d1571d773930c79bc66195998e33f934f464
danielefongo <danielefongo@gmail.com>
2025-03-20 09:59:16 +0100

    feat: use the new treesitter api

--- lua/microscope-files/parsers.lua
+++ lua/microscope-files/parsers.lua
@@ -1,5 +1,6 @@
 local utils = require("microscope-files.utils")
 local highlight = require("microscope.api.highlight")
+local treesitter = require("microscope.api.treesitter")
 
 local parsers = {}
 
@@ -12,10 +13,14 @@ function parsers.file_row_col(data, _)
   local elements = vim.split(data.text, ":", {})
   local limited_path = utils.relative(data.text)
   local line_text = string.gsub(data.text, table.concat({ elements[1], elements[2], elements[3] }, ":"), "")
+  local lang = vim.filetype.match({ filename = elements[1], buf = 0 })
   local highlights = highlight
     .new(data.highlights, limited_path)
     :hl_match(highlight.color.color1, "(.*:)(%d+:%d+:)(.*)", 1)
     :hl_match(highlight.color.color2, "(.*:)(%d+:%d+:)(.*)", 2)
+    :hl_match_with(function(text)
+      return treesitter.for_text(text, lang)
+    end, "(.*:)(%d+:%d+:)(.*)", 3)
     :get_highlights()
 
   return {
@@ -32,8 +37,15 @@ function parsers.row_col(data, request)
   local elements = vim.split(data.text, ":", {})
   local line_text = string.gsub(data.text, table.concat({ elements[1], elements[2] }, ":"), "")
   local filename = vim.api.nvim_buf_get_name(request.buf)
-  local highlights =
-    highlight.new(data.highlights, data.text):hl_match(highlight.color.color2, "(%d+:%d+:)(.*)", 1):get_highlights()
+  local lang = vim.filetype.match({ filename = filename, buf = 0 })
+  local highlights = highlight
+    .new(data.highlights, data.text)
+    :hl_match(highlight.color.color2, "(%d+:%d+:)(.*)", 1)
+    :hl_match_with(function(text)
+      print(text)
+      return treesitter.for_text(text, lang)
+    end, "(%d+:%d+:)(.*)", 2)
+    :get_highlights()
 
   return {
     text = data.text,
--- lua/microscope-files/preview.lua
+++ lua/microscope-files/preview.lua
@@ -1,5 +1,5 @@
 local scope = require("microscope.api.scope")
-local highlight = require("microscope.utils.highlight")
+local treesitter = require("microscope.api.treesitter")
 local utils = require("microscope-files.utils")
 
 local preview = {}
@@ -39,9 +39,12 @@ function preview.cat(data, window)
         flow.consume(flow.cmd.shell("cat", { text }))
       end,
     },
-    callback = function(lines, text)
+    callback = function(lines)
       window:write(lines)
-      highlight(text, window.buf)
+
+      local hls = treesitter.for_buffer(window.buf, vim.filetype.match({ filename = data.file, buf = window.buf }))
+      window:clear_buf_hls()
+      window:set_buf_hls(hls)
       window:set_cursor(cursor)
       if cursor[2] > 0 then
         window:set_buf_hl("Search", cursor[1], cursor[2], cursor[2])
